1a9a44d145e4ee547c5203ea5f423e6de94f05c8
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-11-28 08:11:50 -0800

    Create console.py

---
+++ console.py
@@ -0,0 +1,14 @@
+guillaume@ubuntu:~/AirBnB$ ./console.py
+(hbnb) help
+
+Documented commands (type help <topic>):
+========================================
+EOF  help  quit
+
+(hbnb)
+(hbnb) help quit
+Quit command to exit the program
+
+(hbnb)
+(hbnb)
+(hbnb) quit
